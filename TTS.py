b1ec24ca317bb9b5905c8fd7befc8f04dbb749ef
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-02-16 17:42:14 -0700

    Update TTS.py - Add additional parameters
    - Add support for the speed and response_format parameters.
    - Re-arrange some settings
    - Make default file name include voice name

--- TTS.py
+++ TTS.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# For more information about the TTS API, see: https://platform.openai.com/docs/guides/text-to-speech
+
 # ======================================================================================================================================
 # ========================================================= USER SETTINGS ==============================================================
 # ======================================================================================================================================
 
-# Speech file base name - numbers will be appended to this for each file added
-speech_file_base_name = "speech"
-outputFolder = "TTS-Outputs"
-
-model = "tts-1-hd" # "tts-1" or "tts-1-hd"
-voice = "alloy" # alloy, echo, fable, onyx, nova, shimmer
+model = "tts-1-hd"      # "tts-1" or "tts-1-hd"
+voice = "alloy"         # alloy, echo, fable, onyx, nova, shimmer
 text = "This is what I'm going to say!"
+
+# Advanced settings
+format = "mp3"          # "mp3", "opus", "aac", "flac"
+speed = 1.0             # 0.25 to 4.0
+
+# Speech file base name - numbers will be appended to this for each file added
+outputFolder = "TTS-Outputs"
+speech_file_base_name = f"speech_{voice}"  # Example: speech_tts-1-hd_alloy
 
 # ======================================================================================================================================
 # ======================================================================================================================================
@@ -41,7 +47,9 @@ client = OpenAI(api_key=load_api_key())  # Retrieves key from key.txt file
 response = client.audio.speech.create(
   model=model,
   voice=voice,
-  input=text
+  input=text,
+  response_format=format,
+  speed=speed
 )
 
 # Create outputFolder
@@ -49,10 +57,10 @@ if not os.path.exists(outputFolder):
     os.makedirs(outputFolder)
 
 # Determine file name by finding the next available number out of files in the outputFolder. Starting with no number then starting at 2
-file_name = f"{speech_file_base_name}.mp3"
+file_name = f"{speech_file_base_name}.{format}"
 file_number = 2
 while os.path.exists(os.path.join(outputFolder, file_name)):
-    file_name = f"{speech_file_base_name}_{file_number}.mp3"
+    file_name = f"{speech_file_base_name}_{file_number}.{format}"
     file_number += 1
     
 # Save the audio to a file
